08341723add000c237056cabff5232199e0bb475
gopalindians <!@#QWe345>
2015-01-03 02:29:46 +0530

    Beautified the output.

--- PythonIntro/2.py
+++ PythonIntro/2.py
@@ -1,6 +1,30 @@
 __author__ = 'gopalindians'
+
+print("\nExercises on Comments  http://www.afterhoursprogramming.com/tutorial/Python/Comments/\n")
+
 #Comments File
+
+print("Comments in Python is of two types.\n")
+print("Multi-line and Single-line")
+
+print("-------------------------------------------------")
+
+# Multi-line
+
+print("Multi-line comments with ''' '''")
+print("Example: '''Dummy Links'''")
+
+
 '''
 Hanji
 '''
-print("This is not a comment")
+
+print("-------------------------------------------------")
+
+# v2
+
+print("Single line comment with #")
+print("Comment example:  #Beautiful single line comment ")
+
+print("-------------------------------------------------")
+
