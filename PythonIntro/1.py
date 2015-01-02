8e52e2e5f0fb6979a91a6d269447496699bf506d
gopalindians <!@#QWe345>
2015-01-03 02:33:22 +0530

    Beautified the output.

--- PythonIntro/1.py
+++ PythonIntro/1.py
@@ -1,3 +1,22 @@
 __author__ = 'gopalindians'
-print("Jai ")  # print() similar to printf() in c
+
+print("Exercises on print()  http://www.afterhoursprogramming.com/tutorial/Python/Introduction/")
+
+# Print
+
+print("\nCommand: print(\"Jai\")\n\t\t print(\"mata di\")\n")
+
+print("Outputs:")
+
+print("Jai")  # print() similar to printf() in c
 print("mata di")
+
+print("-------------------------------------------------")
+
+# v2
+print("Same output but command with some modification\n")
+print("Command: print(\"Jai\\nmata di\")\n")  #Same as above but in single line
+
+print("Outputs:")
+print("Jai\nmata di")
+print("-------------------------------------------------")
